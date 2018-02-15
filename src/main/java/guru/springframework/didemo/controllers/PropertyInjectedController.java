7536331a79bc5f2a99299ad3a9cdb86836d88d52
jperezdelafuente <jesusperez82@gmail.com>
2018-02-15 21:02:53 +0100

    adding Primary Bean

--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -3,12 +3,16 @@ package guru.springframework.didemo.controllers;
 import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
+    // without @Qualifier is injected the Primary bean
+    // without @Primary is injected the greetingServiceImpl bean
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
