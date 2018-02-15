c101490f78b9f236b5a53e0f9f08e07b5973ada2
jperezdelafuente <jesusperez82@gmail.com>
2018-02-15 20:15:08 +0100

    adding Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
+import guru.springframework.didemo.controllers.PropertyInjectedController;
+import guru.springframework.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,9 +12,13 @@ import org.springframework.context.ApplicationContext;
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctc = SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctc.getBean("myController");
+		MyController controller = (MyController) ctx.getBean("myController");
 		controller.hello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -1,11 +1,15 @@
 package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    // Optional in constructor since Spring Framework 4.2 -> @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
