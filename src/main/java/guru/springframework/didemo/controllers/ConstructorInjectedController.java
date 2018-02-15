d9be6f94bebbb14fbf86b147b374c8c6913e7ba6
jperezdelafuente <jesusperez82@gmail.com>
2018-02-15 20:40:27 +0100

    adding more services for Qualifier example

--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,7 +11,7 @@ public class ConstructorInjectedController {
     private GreetingService greetingService;
 
     // Optional in constructor since Spring Framework 4.2 -> @Autowired
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
