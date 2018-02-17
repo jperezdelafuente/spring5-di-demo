fda6113ffb19271c6d655cc1e8ec4437134d9177
jperezdelafuente <jesusperez82@gmail.com>
2018-02-17 16:11:07 +0100

    adding factory bean example

--- src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service
-@Profile("de")
-@Primary
+//@Service
+//@Profile("de")
+//@Primary
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
